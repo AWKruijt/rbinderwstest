3f27d2e1a7a3cb0d4fe31cebdb8056277f141509
Anne-Wil <mail@awkruijt.nl>
2019-07-08 14:29:38 +0200

    Add files via upload

--- ch 1.R
+++ ch 1.R
@@ -1,6 +1,3 @@
-require(ggplot2)
-require(readr)
-
 full_trains <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
 
 ggplot(full_trains, aes(x= journey_time_avg)) + 
